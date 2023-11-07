97401f3164d246a4778c13fa95251d0c3084c397
Lukas Dembicki <lad_777@hotmail.com>
2023-11-07 17:13:37 +0000

    Form done.

--- src/components/NewContact.jsx
+++ src/components/NewContact.jsx
@@ -1,10 +1,54 @@
-
+import { useState } from "react"
 
 function NewContact() {
 
+    const [newContact, setNewContact] = useState(
+        {
+            firstName: '',
+            lastName: '',
+            city: '',
+            street: ''
+        }
+    )
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setNewContact({ ...newContact, [name]: value })
+    }
+
     return (
         <>
-            <p>New Contact Form</p>
+            <form action="">
+                <label htmlFor="firstName">First Name:
+                    <input
+                        value={newContact.firstName}
+                        type="text"
+                        name="firstName"
+                        onChange={(event) => { handleChange(event) }} />
+                </label><br />
+                <label htmlFor="lastName">Last Name:
+                    <input
+                        value={newContact.lastName}
+                        type="text"
+                        name="lastName"
+                        onChange={(event) => { handleChange(event) }} />
+                </label><br />
+                <label htmlFor="city">City:
+                    <input
+                        value={newContact.city}
+                        type="text"
+                        name="city"
+                        onChange={(event) => { handleChange(event) }} />
+                </label><br />
+                <label htmlFor="street">Street:
+                    <input
+                        value={newContact.street}
+                        type="text"
+                        name="street"
+                        onChange={(event) => { handleChange(event) }} />
+                </label><br />
+                <button type="submit">Create new contact</button>
+            </form>
         </>
     )
 }
