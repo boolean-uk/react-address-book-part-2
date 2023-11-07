66ba525e2bfe8febd4423f4c45dccabdcd6e15fa
callumhayden <callum.hayden@gmail.com>
2023-11-07 14:37:11 +0000

    app foundation completed with routes/route/links applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom"
+import ContactList from "./components/ContactList";
+import ContactListItem from "./components/ContactListItem";
+import ContactForm from "./components/ContactForm";
 import './styles/App.css';
 
 function App() {
 
+    const [individual, setindividual] = useState('')
 
-    
+
     return (
-        <p>Hello, world!</p>
+       <>
+       <h1>Menu</h1>
+       <nav>
+       <ul>
+        <Link to="/"><li>Contact list</li></Link>
+        <Link to="/contact/form"><li>Add new contacts</li></Link>
+       </ul>
+       </nav>
+       <main>
+        <Routes>
+            <Route path="/" element={<ContactList />}></Route>
+            <Route path="/contact/Form" element={<ContactForm />} ></Route>
+            <Route path="/:id" element={<ContactListItem />}></Route>
+        </Routes>
+       </main>
+       </>
     );
 }
 
