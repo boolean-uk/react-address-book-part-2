6cffce35d501e4ea27cf5f020ef5666d9ff7cf1a
TomEastwood <t.eastwood99@gmail.com>
2023-11-07 18:04:05 +0000

    form.jsx, form.css and AddContact.jsx semi made

---
+++ src/components/AddContact.jsx
@@ -0,0 +1,17 @@
+import Form from "./Form/Form";
+
+function AddContact () {
+    return (
+        <div className = "add-contact-container">
+            <h2>Add a new Contact</h2>
+            <Form 
+            // firstName = {firstName}
+            // lastName = {lastName}
+            // street = {street}
+            // city = {city}
+            />
+        </div>
+    )
+}
+
+export default AddContact;
