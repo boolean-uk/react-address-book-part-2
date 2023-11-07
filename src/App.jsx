5e682d41fa88dd4f2862b065c6f88f4fbceaf8ab
Rebecca Noy <rebecca.noy@gmx.de>
2023-11-07 17:10:14 +0100

    set up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import './App.css';
+import { Routes, Route, Link }
+import { useNavigate } from 'react-router-dom';
 
 function App() {
     return (
-        <p>Hello, world!</p>
+        <>
+        <aside>
+            <nav>
+                <li>
+                    <Link to="/">Home</Link>
+                    <Link to="/add">Add Contact</Link>
+                </li>
+            </nav>
+        </aside>
+
+
+        <Routes>
+            <Route
+            path="/"
+            element={<Dashboard />}
+            />
+            <Route
+            path="/view/:id"
+            element={<ContactDetails />}
+            />
+            <Route
+            path="/edit/:id"
+            element={<EditContact />}
+            />
+            <Route
+            path="/add"
+            element={<AddContact />}
+            />
+        </Routes>
+      </>
     );
 }
 
