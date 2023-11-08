211c7c4cae1e12fefe51b4fb9bf6166fcdb60f8a
Mobeen <muhammadmobeen1@hotmail.com>
2023-11-08 19:50:27 +0100

    done with Callum

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function ContactList() {
+  const [individualData, setIndividualData] = useState([]);
+
+  function fetchWork() {
+    fetch("https://boolean-api-server.fly.dev/Callumhayden99/contact")
+      .then((response) => response.json())
+      .then((data) => setIndividualData(data));
+  }
+  useEffect(fetchWork, []);
+
+  return (
+    <>
+      <header>
+        <h1 className="contact-h1">Contacts</h1>
+      </header>
+
+      <ul className="contact-list">
+        {individualData.map((contact) => (
+          <li className="contact" key={contact.id}>
+            <div className="name">Name:</div>
+            <div className="name-tag">
+              {contact.firstName} {contact.lastName}
+            </div>
+            <div className="link">
+              <Link to={`/contact/${contact.id}`}>View</Link>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ContactList;
