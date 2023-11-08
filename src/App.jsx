21a5e290dc10d0a307a299ee52990a4ffd907084
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-08 21:37:39 +0100

    Added some css

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import "./App.css";
 
 import ContactList from "./components/Menu/Contact/ContactList";
 import CreateContact from "./components/Menu/CreateContact/CreateContact";
+import Home from "./components/Menu/Home/Home";
 function App() {
   return (
     <>
       <div className="app">
         <header>
           <h2>Menu</h2>
+          <Link to={"/"}>Home</Link>
+          <br />
           <Link to={"/Contact-List"}>Contact List</Link>
           <br />
           <Link to={"/Create-New-Contact"}>Add New Contact</Link>
         </header>
         <div className="routes">
           <Routes>
-            <Route path="/Contact-List" element={<ContactList />}></Route>
+            <Route path="/" element={<Home/>}
+            ></Route>
+            <Route 
+            path="/Contact-List" 
+            element={<ContactList />}
+            ></Route>
             <Route
               path="/Create-New-Contact"
               element={<CreateContact />}
