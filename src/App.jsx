43b1cfec1d841b29329df87f7ebc98fd76394cd3
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-08 21:17:35 +0100

    made the links and paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
 
+import ContactList from "./components/Menu/Contact/ContactList";
+import CreateContact from "./components/Menu/CreateContact/CreateContact";
 function App() {
-    return (
-        <p>Hello, world!</p>
-    );
+  return (
+    <>
+      <div className="app">
+        <header>
+          <h2>Menu</h2>
+          <Link to={"/Contact-List"}>Contact List</Link>
+          <br />
+          <Link to={"/Create-New-Contact"}>Add New Contact</Link>
+        </header>
+        <div className="routes">
+          <Routes>
+            <Route path="/Contact-List" element={<ContactList />}></Route>
+            <Route
+              path="/Create-New-Contact"
+              element={<CreateContact />}
+            ></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Menu/Contact/ContactList.jsx
@@ -0,0 +1,13 @@
+
+
+function ContactList(props) {
+    const {data} = props
+
+  return (
+    <div className="contact-list">
+        <li>Ayush lama</li>
+    </div>
+  )
+}
+
+export default ContactList
