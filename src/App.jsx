a689faf6ca02a935352ed5ac564ce0fb655d9b7e
LAVINIABENZAR <loredanabenzar2@gmail.com>
2023-11-09 09:01:43 +0000

    address-book

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Contacts from './components/Contacts';
+import NewContact from './components/NewContact';
+
+//https://boolean-api-server.fly.dev/LAVINIABENZAR/contact
+
+
 
 function App() {
     return (
-        <p>Hello, world!</p>
+        <BrowserRouter>
+      <ul className='home-link'>
+                <li><Link className="nav-link" to={'/'}>Home</Link></li>
+                <li><Link className="nav-link" to={'/contacts'}>Contacts</Link></li>
+                <li><Link className="nav-link" to={'/newcontact'}>Add New Contact</Link></li>
+     </ul>
+      
+       <Routes>
+
+       <Route 
+        path='/'
+        element={<Home/>}
+        />
+
+        <Route
+        path='/contacts'
+        element={<Contacts/>}
+        />
+
+        <Route
+        path='/newcontact'
+        element={<NewContact/>}/>
+
+        <Route
+        path='/contact/:id'
+        element={<Contact/>}/>
+
+       </Routes>
+    
+        </BrowserRouter>
+    
+        
     );
 }
 
---
+++ src/components/NewContact.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { URL, get, post } from "./Client";
+import { useNavigate } from "react-router-dom";
+
+
+function NewContact() {
+
+    const navigate = useNavigate()
+
+    const [newContact, setNewContact] = useState(
+        {
+            firstName: '',
+            lastName: '',
+            city: '',
+            street: ''
+        }
+    )
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setNewContact({ ...newContact, [name]: value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        post(`${URL}`, newContact)
+        .then((res)=> res.json())
+        .then(get(URL))
+        .then(navigate("/contacts"))
+    }
+
+    return (
+        <>
+            <form 
+            className="newContactForm"
+            onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name:
+                    <input
+                        value={newContact.firstName}
+                        type="text"
+                        name="firstName"
+                        onChange={(event) => { handleChange(event,) }} />
+                </label>
+                <br />
+                <label htmlFor="lastName">Last Name:
+                    <input
+                        value={newContact.lastName}
+                        type="text"
+                        name="lastName"
+                        onChange={(event) => { handleChange(event) }} />
+                </label>
+                <br />
+                <label htmlFor="city">City:
+                    <input
+                        value={newContact.city}
+                        type="text"
+                        name="city"
+                        onChange={(event) => { handleChange(event) }} />
+                </label>
+                <br />
+                <label htmlFor="street">Street:
+                    <input
+                        value={newContact.street}
+                        type="text"
+                        name="street"
+                        onChange={(event) => { handleChange(event) }} />
+                </label>
+                <br />
+                <button 
+                className="add-contact-button"
+                type="submit"
+                > Add New Contact
+                    </button>
+            </form>
+        </>
+    )
+}
+
+export default NewContact;
