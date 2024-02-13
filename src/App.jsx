a55a8811e6d561746bb393e1703f95d5b4380f67
Radio58 <rt58gm@gmail.com>
2024-02-13 10:49:33 +0000

    added contact form

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
                 />
                 <Route
                 path="/add-contact"
-                element={<ContactForm/>}
+                element={<ContactForm contacts={contacts} setContacts={setContacts}/>}
                 />
             </Routes>
         </main>
