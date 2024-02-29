4355941df76035185533da94b13733c54466ff29
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-29 13:50:49 +0100

    extension 7

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,11 +1,45 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function ContactList({ contacts }) {
+  const [showFilterForm, setShowFilterForm] = useState(false);
+  const [filterInput, setFilterInput] = useState("");
+
+  const toggleFilterForm = () => {
+    setShowFilterForm((prev) => !prev);
+  };
+
+  const handleChange = (e) => {
+    setFilterInput(e.target.value);
+  };
+
+  const handleFilter = () => {
+    // Filter contacts based on the filter input
+    const filteredContacts = contacts.filter((contact) =>
+      contact.firstName.toLowerCase().includes(filterInput.toLowerCase())
+    );
+    return filteredContacts;
+  };
+
   return (
     <div>
       <h2>Contact List</h2>
+      <button onClick={toggleFilterForm}>Filter</button>
+      {showFilterForm && (
+        <form>
+          <label>
+            First Name:
+            <input
+              type="text"
+              name="filterInput"
+              value={filterInput}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      )}
       <ul style={{ marginBottom: "10px" }}>
-        {contacts.map((contact) => (
+        {handleFilter().map((contact) => (
           <ContactListItem key={contact.id} contact={contact} />
         ))}
       </ul>
