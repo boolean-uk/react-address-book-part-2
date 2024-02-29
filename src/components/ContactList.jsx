030aeb78f575527b07535d1fb4745bc08d0cf245
Klara Andersson <klara.andersson@se.experis.com>
2024-02-29 12:13:23 +0100

    Coding done

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ContactList(props)
 {
     const { contacts } = props
+    const [searchTitle, setSearchTitle] = useState("")
+    let filteredContacts = contacts
+
+    if (searchTitle.length > 0)
+    {
+        filteredContacts = filteredContacts.filter((contact) => 
+        {
+            if (contact.firstName.toLowerCase().includes(searchTitle.toLowerCase())) return contact
+            else if (contact.lastName.toLowerCase().includes(searchTitle.toLowerCase())) return contact
+        })
+    }
+
+    const handleFiltering = (event) =>
+    {
+        setSearchTitle(event.target.value)
+    }
 
     return (
         <>
         <h1>Contacts</h1>
+        <h3>Filter</h3>
+        <input type='text' value={searchTitle} onChange={handleFiltering} />
         <ul>
-          {contacts.map((contact, index) =>
+          {filteredContacts.map((contact, index) =>
           (
             <li
             key={index}>{contact.firstName} {contact.lastName}
