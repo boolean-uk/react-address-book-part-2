acd29b49ebe414735de7ed4bef5b0b06466bb384
Mårten Elfving <Marten.elfving@gmail.com>
2024-02-29 15:15:18 +0100

    ext+core: nonWorking interfaceUpdate on update contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,26 @@ import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
 import ContactList from "./components/ContactList/ContactList";
 import { useEffect, useState } from "react";
 import SingleContactView from "./components/SingleContactView/SingleContactView";
-import AddContactForm from "./components/AddContactForm/AddContactForm";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
   const [contacts, setContacts] = useState([]);
 
-  const processData = (contactArray) => {
-    return contactArray.map((element, index) => ({ ...element, id: index }));
-  };
-
   const fetchContacts = async function () {
+    console.log("Fetching conctacts");
     const url = "https://boolean-api-server.fly.dev/martenere/contact";
     try {
       let result = await fetch(url);
       let data = await result.json();
-      const processedContacts = processData(data);
-      console.log(processedContacts);
+      //   const processedContacts = processData(data);
+      //   console.log(processedContacts);
 
-      setContacts(processedContacts);
+      setContacts(data);
     } catch (error) {
       console.log(error);
     }
   };
-  useEffect(() => fetchContacts, []);
+  //   useEffect(() => fetchContacts, []);
 
   return (
     <div className="Main-View">
@@ -42,9 +39,14 @@ function App() {
         />
         <Route
           path="/contacts/:id"
-          element={<SingleContactView contacts={contacts} />}
+          element={
+            <SingleContactView
+              contacts={contacts}
+              fetchContacts={fetchContacts}
+            />
+          }
         />
-        <Route path="/AddContact" element={<AddContactForm />} />
+        <Route path="/AddContact" element={<ContactForm method={"POST"} />} />
       </Routes>
     </div>
   );
--- src/components/SingleContactView/SingleContactView.jsx
+++ src/components/SingleContactView/SingleContactView.jsx
@@ -1,41 +1,79 @@
+import { useEffect, useState } from "react";
 import "./SingleContactView.css";
-import { useParams } from "react-router-dom";
-function SingleContactView({ contacts }) {
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import ContactForm from "../ContactForm/ContactForm";
+function SingleContactView({ contacts, fetchContacts }) {
+  const [edit, setEdit] = useState(false);
   const { id } = useParams();
+  // const [contactsTest, fetchContactsTest] = useState(0);
+
+  // useEffect(() => fetchContactsTest("called"), []);
+  // console.log(contactsTest);
+
+  function callFetchContacts() {
+    console.log("Calling fetchContacts");
+    fetchContacts();
+  }
+  useEffect(() => {
+    callFetchContacts();
+  }, []);
   const contact = contacts.find((contact) => contact.id.toString() === id);
+  const navigate = useNavigate();
+  const sendDeleteRequest = async () => {
+    const deleteEndpoint = `https://boolean-api-server.fly.dev/martenere/contact/${id}`;
+    const options = {
+      method: "DELETE",
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+    let res = await fetch(deleteEndpoint, options);
+    let data = await res.json();
+    console.log(data);
+    navigate("/contacts");
+  };
 
   if (!contact) {
     return <div className="contact-not-found">Contact not found</div>;
   }
   return (
-    <div className="contact-view">
-      <img
-        src={contact.profileImage}
-        alt={`${contact.firstName} ${contact.lastName}`}
-        className="contact-image"
-      />
-      <h2>
-        {contact.firstName} {contact.lastName}
-      </h2>
-      <p>
-        <strong>Email:</strong> {contact.email}
-      </p>
-      <p>
-        <strong>Job Title:</strong> {contact.jobTitle}
-      </p>
-      <p>
-        <strong>Address:</strong> {contact.street}, {contact.city}
-      </p>
-      <p>
-        <strong>Coordinates:</strong> {contact.latitude}, {contact.longitude}
-      </p>
-      <p
-        style={{ backgroundColor: contact.favouriteColour }}
-        className="favourite-colour"
-      >
-        Favourite Colour
-      </p>
-    </div>
+    <>
+      <div className="contact-view">
+        <img
+          src={contact.profileImage}
+          alt={`${contact.firstName} ${contact.lastName}`}
+          className="contact-image"
+        />
+        <h2>
+          {contact.firstName} {contact.lastName}
+        </h2>
+        <p>
+          <strong>Email:</strong> {contact.email}
+        </p>
+        <p>
+          <strong>Job Title:</strong> {contact.jobTitle}
+        </p>
+        <p>
+          <strong>Address:</strong> {contact.street}, {contact.city}
+        </p>
+        <p>
+          <strong>Coordinates:</strong> {contact.latitude}, {contact.longitude}
+        </p>
+        <p
+          style={{ backgroundColor: contact.favouriteColour }}
+          className="favourite-colour"
+        >
+          Favourite Colour
+        </p>
+        <button onClick={sendDeleteRequest}>Delete</button>
+        <button onClick={() => setEdit(!edit)}>Edit</button>
+      </div>
+      {edit && (
+        <>
+          <ContactForm method="PUT" contact={contact} />
+        </>
+      )}
+    </>
   );
 }
 
