604c0aa35202ddb5e780fbabcb7ac0d078758f27
Victor <victor.adamson@se.experis.com>
2024-03-01 15:20:49 +0100

    Now you can delete contacts

--- src/components/ContactInfoPage.jsx
+++ src/components/ContactInfoPage.jsx
@@ -1,11 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function ContactInfoPage({ contacts }) {
   //Using parameters get the right contact
+  const navigate = useNavigate();
   const { id } = useParams();
   const contact = contacts.find((contact) => contact.id === parseInt(id));
   if (!contact) return <p>Loading...</p>;
+  const deleteRequestOptions = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(contact),
+  };
 
+  function deleteContact() {
+    fetch(
+      `https://boolean-api-server.fly.dev/VictorAdamson/contact/${id}`,
+      deleteRequestOptions
+    ).then((response) => {
+      response.json();
+      navigate("/contacts");
+    });
+  }
   return (
     <>
       <section className="contact-list-box">
@@ -15,6 +32,7 @@ function ContactInfoPage({ contacts }) {
         </p>
         <p>City: {contact.city}</p>
         <p>Street: {contact.street}</p>
+        <button onClick={deleteContact}>Delete Contact</button>
       </section>
     </>
   );
