b4be6c0b21abf8e8373eefbc754d1ca275f146ae
Sabbasn <ben_7aus@hotmail.com>
2024-03-01 11:31:44 +0100

    Implement core criteria

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="ab-menu">
+      <h1>Menu</h1>
+      <Link to={"/"}>Contacts List</Link>
+      <Link to={"/contact-form"}>Add New Contact</Link>
+    </nav>
+  );
+}
