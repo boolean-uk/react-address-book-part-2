bd1d8430c123731834f77f833d851b30c278d9a0
spectraldesign <spectraldesign1@gmail.com>
2024-03-01 12:38:43 +0100

    Exension 1

--- src/pages/ContactDetails.jsx
+++ src/pages/ContactDetails.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import '../styles/ContactDetails.css'
 
 export default function ContactDetails() {
     const id = useParams().id
     const [contact, setContact] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`https://boolean-api-server.fly.dev/spectraldesign/contact/${id}`)
@@ -17,12 +18,23 @@ export default function ContactDetails() {
             <p>Loading...</p>
         </div>
     )
+
+    const handleDelete = () => {
+        fetch(`https://boolean-api-server.fly.dev/spectraldesign/contact/${id}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then(() => navigate('/'))
+    }
+
     return (
         <div className="infoContainer">
             <h1 className="title">Contact Details</h1>
             <h2 className="name">Name: {contact.firstName} {contact.lastName}</h2>
             <p className="street">Street: {contact.street}</p>
             <p className="city">City: {contact.city}</p>
+            <button className="deleteButton" onClick={handleDelete}>Delete</button>
         </div>
     )
 }
