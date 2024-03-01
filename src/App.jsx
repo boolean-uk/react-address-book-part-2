59cd86f501f5f4658f9c457cb97b352811e88fa2
Matthias Greeven <matsgreeven@gmail.com>
2024-03-01 11:17:15 +0100

    Initial Setup of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import './App.css';
+import { useState,useEffect } from 'react';
+import { Link,
+Route,
+Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import ContactProfile from './Pages/ContactProfile/ContactProfile';
+const URL = "https://boolean-api-server.fly.dev/thegrevling/contact"
 
 function App() {
+    const [contacts,setcontacts] = useState([])
+
+    useEffect(() => {
+        fetch(URL)
+        .then(res => res.json())
+        .then(setcontacts)
+    }, [])
+    useEffect(()=>{
+        console.log(contacts)
+    },[contacts])
+
     return (
-        <p>Hello, world!</p>
+    <>
+      <header>
+        <h1>Hire Your Team</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Dashboard</Link></li>
+          </ul>
+        </nav>
+      </header> 
+      <Routes>
+        <Route
+            path="/"
+            element={<Dashboard/>}
+          />
+          <Route path="/view/:id" element={<ContactProfile />} />
+      </Routes>
+    </>
     );
 }
 
---
+++ src/Pages/ContactProfile/ContactProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactProfile() {
+  return (
+    <div>ContactProfile</div>
+  )
+}
+
+export default ContactProfile
