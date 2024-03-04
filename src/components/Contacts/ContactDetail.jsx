873a3bb4d24aed79f36a8e20bbeac2b64613aaf9
Gudbrand <gudbrandwd@live.no>
2024-03-04 10:53:26 +0100

    added google maps embed to display contact coordinates

--- src/components/Contacts/ContactDetail.jsx
+++ src/components/Contacts/ContactDetail.jsx
@@ -2,7 +2,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 const ContactDetail = ({ contacts, setContacts }) => {
     const { id } = useParams();
-    const person = contacts.find((p) => p.id === parseInt(id));
+    const contact = contacts.find((p) => p.id === parseInt(id));
 
     const nav = useNavigate();
 
@@ -15,11 +15,11 @@ const ContactDetail = ({ contacts, setContacts }) => {
 
     const handleDeletion = () => {
         fetch(
-            "https://boolean-api-server.fly.dev/BloodyFM/contact/" + person.id,
+            "https://boolean-api-server.fly.dev/BloodyFM/contact/" + contact.id,
             ApiDeleteRequest
         );
 
-        const newContacts = contacts.filter((p) => p.id !== person.id);
+        const newContacts = contacts.filter((p) => p.id !== contact.id);
         setContacts([...newContacts]);
 
         nav("/");
@@ -31,10 +31,15 @@ const ContactDetail = ({ contacts, setContacts }) => {
 
     return (
         <>
-            <h2>{person.firstName + " " + person.lastName}</h2>
-            <p>{person.street + " " + person.city}</p>
+            <h2>{contact.firstName + " " + contact.lastName}</h2>
+            <p>{contact.street + " " + contact.city}</p>
             <button onClick={goToEdit}>Edit</button>
             <button onClick={handleDeletion}>Delete</button>
+            <iframe
+                width="100%"
+                height="250"
+                src={`https://maps.google.com/maps?q=${contact.latitude}, ${contact.longitude}&output=embed`}
+            ></iframe>
         </>
     );
 };
