939a86acac3210f507e9062403f9df904214723e
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-04 10:22:13 +0100

    Extension 6 - Updated map component

--- src/components/ContactDetails/MapComponent/MapComponent.jsx
+++ src/components/ContactDetails/MapComponent/MapComponent.jsx
@@ -1,40 +1,12 @@
-import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
-
-const libraries = ['places'];
-const mapContainerStyle = {
-  width: '60vw',
-  height: '60vh',
-};
-
-
 const MapComponent = ({lat, lng}) => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
-    libraries,
-  });
-
   const center = {
     lat: parseFloat(lat) || 63.5, // default latitude
     lng: parseFloat(lng) || 11, // default longitude
   };
 
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
-
-  if (!isLoaded) {
-    return <div>Loading maps</div>;
-  }
-
   return (
     <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={5}
-        center={center}
-      >
-        <MarkerF position={center} />
-      </GoogleMap>
+      <iframe width="100%" height="100%" src={`https://maps.google.com/maps?q=${center.lat},${center.lng}&z=7&output=embed`} loading="lazy"></iframe>
     </div>
   );
 };
