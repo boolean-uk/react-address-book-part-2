0fedc38430a1ceabbeadbaa38ad4a5fe7521a5df
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-04 10:04:55 +0100

    Extension 7 - Added search field

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,13 +5,26 @@ import { useState } from 'react'
 
 const ContactList = ({contacts, refreshContacts}) => { 
     const [modalContent, setModalContent] = useState(undefined)
-    
+    const [searchQuery, setSearchQuery] = useState("")
+
     return (
         <div className="content-container scroll-container">
             <div className="title">Contact list</div>
+            <div className="search-container">
+                Filter by name:  
+                <input 
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+            </div>
             <ConfirmDeleteAction content={modalContent} refreshContacts={refreshContacts} setContent={setModalContent}/>
             <ul className="contacts-list">
-                {contacts.map((contact, index) => (
+                {contacts.filter((c) => {
+                    const searchTerms = searchQuery.toLowerCase().split(" ")
+                    return searchTerms.every((t) => (
+                        c.firstName.toLowerCase().includes(t) || c.lastName.toLowerCase().includes(t)
+                    ))})
+                    .map((contact, index) => (
                     <li key={index}>
                         <ContactListItem contact={contact} confirmDelete={setModalContent}/>
                     </li>
