2e131434cd011346cc458650b69888cd0c7c7e3b
Hiko Yüksek <33344671+hkyksk@users.noreply.github.com>
2024-03-07 23:00:01 +0100

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import ContactList from './components/ContactList'
+import NewContact from './components/NewContactForm'
+
 import './App.css';
 
 function App() {
+    const [contacts, setContacts] = useState("")
+    const [createContact, setCreateContact] = useState("")
+
+    useEffect(() => {
+        fetch("https://boolean-api-server.fly.dev/hkyksk/contact")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.results)
+            setContacts(data.results)
+          })    
+      }, [])
+
+
     return (
-        <p>Hello, world!</p>
-    );
+        <Router> 
+            <div>
+              <h2>Menu</h2>
+              <nav>
+                <li><Link to="/contact-list">Contact List</Link></li>
+                <li><Link to="/create-contact">Add New Contact</Link></li>
+              </nav>
+              <Routes>
+                  <Route path="/contact-list" element={<ContactList setContacts={setContacts} />} />
+                  <Route path="/create-contact" element={<NewContact />} />
+              </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,7 @@
+import ContactListItem from "./ContactListItem"
+
+export default function ContactList() {
+    return (
+        <p>Hello, world!</p>
+    )
+}
