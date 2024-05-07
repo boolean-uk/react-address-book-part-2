b1be5665428e229086f75e3b353d91aa13aa1ba0
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-08 00:45:07 +0100

    create Toast component

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+export default function Toast() {
+	return (
+		<ToastContainer
+			hideProgressBar
+			newestOnTop
+		/>
+	);
+}
