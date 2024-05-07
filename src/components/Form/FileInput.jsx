d783b334971186afcb488d7a4b84de1f7e6fa6d2
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-07 23:12:37 +0100

    create file input

---
+++ src/components/Form/FileInput.jsx
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import { FormControl, Image } from "react-bootstrap";
+import placeholderImg from "../../media/user-image-placeholder.png";
+import styles from "./file-input.module.css";
+export default function FileInput({ value, onChange }) {
+	const [src, setSrc] = useState(value || placeholderImg);
+
+	const handleImageUpload = (e) => {
+		const img = e.target.files[0];
+		setSrc(URL.createObjectURL(img));
+		onChange(img);
+	};
+	const ref = useRef(null);
+
+	return (
+		<div
+			className={styles.wrapper}
+			// didnt even googled what aria label should be used xD
+			// points for trying?
+			aria-roledescription="upload-image"
+			onClick={() => ref.current.click()}>
+			<Image
+				src={src}
+				className={styles.image}></Image>
+			<FormControl
+				ref={ref}
+				className="visually-hidden "
+				type="file"
+				value={value}
+				onChange={handleImageUpload}></FormControl>
+		</div>
+	);
+}
