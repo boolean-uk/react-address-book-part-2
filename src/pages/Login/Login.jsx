62375aa0beffca7e1e185a5b226cce6780d4cc2e
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-07 23:33:31 +0100

    style login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,9 @@ import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTE_NAMES } from "../../routes/router";
 import AuthCtx from "../../stores/auth/auth-store";
-
+import Input from "../../components/Form/Input";
+import { Button, Container, Form } from "react-bootstrap";
+import styles from "./login.module.css";
 export default function Login() {
 	const navigate = useNavigate();
 	const { isLoggedIn, login } = useContext(AuthCtx);
@@ -13,10 +15,29 @@ export default function Login() {
 		}
 	}, [isLoggedIn]);
 
+	const loginHandler = () => {
+		login();
+		//display toast
+	};
+
 	return (
-		<div>
-			Login
-			<button onClick={login}>Log in</button>
-		</div>
+		<Container className={styles.wrapper}>
+			<h1 className="mb-4">Address Book</h1>
+			<Form>
+				<Input
+					id={"name"}
+					label={"Username"}></Input>
+				<Input
+					id={"password"}
+					label={"Password"}
+					type={"password"}></Input>
+			</Form>
+			<Button
+				className="mt-2"
+				size="lg"
+				onClick={loginHandler}>
+				Log in
+			</Button>
+		</Container>
 	);
 }
