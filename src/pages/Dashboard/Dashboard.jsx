a91a392b2a305b428fbdb954913ea1169a149603
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-08 10:00:29 +0100

    implement list filters

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion, Container, Stack } from "react-bootstrap";
 import UserNav from "./UserNav";
 import { Outlet, useLoaderData } from "react-router-dom";
 import AccordionList from "../../components/AccordionList/AccordionList";
+import ListFilters from "./ListFilters";
 
 export const dashboardRouteLoader = () =>
 	fetch("https://boolean-api-server.fly.dev/rafa-lopes-pt/contact");
 export default function Dashboard() {
 	const loaderData = useLoaderData();
+
 	console.log(loaderData);
+	const [filteredList, setFilteredList] = useState(loaderData);
 	return (
 		<Container fluid>
 			<Outlet></Outlet>
 			<Stack>
 				<UserNav />
-				<AccordionList list={loaderData || []} />
+                <ListFilters list={loaderData} onUpdate={setFilteredList}/>
+				<AccordionList list={filteredList} />
 			</Stack>
 		</Container>
 	);
---
+++ src/pages/Dashboard/ListFilters.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { Container, Stack } from "react-bootstrap";
+import Input from "../../components/Form/Input";
+
+export default function ListFilters({ list, onUpdate }) {
+	const [filteredList, setFilteredList] = useState(list);
+
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [city, setCity] = useState("");
+	const [job, setJob] = useState("");
+
+	const handleFiltering = () => {
+		const createFilter = (search) =>
+			search ? (str) => new RegExp(search, "i").test(str) : () => true;
+		console.log(createFilter());
+		const filterName = createFilter(name),
+			filterEmail = createFilter(email),
+			filterCity = createFilter(city),
+			filterJob = createFilter(job);
+
+		setFilteredList(
+			list.filter(
+				(e) =>
+					filterName(`${e.firstName} ${e.firstName}`) &&
+					filterEmail(e.email) &&
+					filterCity(e.city) &&
+					filterJob(e.jobTitle)
+			)
+		);
+	};
+
+	useEffect(() => onUpdate(filteredList), [filteredList]);
+	useEffect(() => {
+		handleFiltering();
+	}, [name, email, city, job]);
+
+	return (
+		<Stack
+			direction="horizontal"
+			gap={2}>
+			<Input
+				id={"name"}
+				label="Name"
+				onChange={(e) => {
+					setName(e.target.value);
+				}}
+				value={name}></Input>{" "}
+			<Input
+				id={"email"}
+				label="Email"
+				type={"email"}
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
+				value={email}></Input>
+			<Input
+				id={"city"}
+				label="City"
+				onChange={(e) => {
+					setCity(e.target.value);
+				}}
+				value={city}></Input>
+			<Input
+				id={"job"}
+				label="Job"
+				onChange={(e) => {
+					setJob(e.target.value);
+				}}
+				value={job}></Input>
+		</Stack>
+	);
+}
