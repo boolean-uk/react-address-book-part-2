e6a30ad5c323a9cc4c3ca7f4718057c4e9a0fb1a
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-08 10:25:11 +0100

    add server action to dashboard route loader

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,20 +4,23 @@ import UserNav from "./UserNav";
 import { Outlet, useLoaderData } from "react-router-dom";
 import AccordionList from "../../components/AccordionList/AccordionList";
 import ListFilters from "./ListFilters";
+import { getAllEntries } from "../../stores/serverActions";
 
-export const dashboardRouteLoader = () =>
-	fetch("https://boolean-api-server.fly.dev/rafa-lopes-pt/contact");
+export const dashboardRouteLoader = getAllEntries;
+//
 export default function Dashboard() {
 	const loaderData = useLoaderData();
 
-	console.log(loaderData);
 	const [filteredList, setFilteredList] = useState(loaderData);
 	return (
 		<Container fluid>
 			<Outlet></Outlet>
 			<Stack>
 				<UserNav />
-                <ListFilters list={loaderData} onUpdate={setFilteredList}/>
+				<ListFilters
+					list={loaderData}
+					onUpdate={setFilteredList}
+				/>
 				<AccordionList list={filteredList} />
 			</Stack>
 		</Container>
