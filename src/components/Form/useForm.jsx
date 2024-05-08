25e6d0d8a6080196f38a57a1cc3a353f147f232b
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-08 12:13:45 +0100

    create useForm hook

---
+++ src/components/Form/useForm.jsx
@@ -0,0 +1,60 @@
+import { useReducer } from "react";
+
+const formDataReducer = (state, action) => {
+	const formData = { ...state };
+	const { name, value, isValid } = action.payload;
+
+	switch (action.action) {
+		case "MUTATE": {
+			formData[name] = { value, isValid };
+			return formData;
+		}
+		case "DELETE": {
+			delete formData[name];
+			return formData;
+		}
+		default:
+			return state;
+	}
+};
+const mutateFormInputAction = (name, value = "", isValid = false) => ({
+	action: "MUTATE",
+	payload: { name, value, isValid },
+});
+const deleteFormInputAction = (name) => ({
+	action: "DELETE",
+	payload: { name },
+});
+
+const useForm = (inputFields) => {
+	const [data, dispatchFormUpdate] = useReducer(formDataReducer, {}, () => {
+		const obj = {};
+		for (const field in inputFields) {
+			if (Array.isArray(inputFields)) {
+				obj[inputFields[field]] = { value: "", isValid: false };
+			} else {
+				obj[field] = inputFields[field];
+			}
+		}
+		return obj;
+	});
+	function mutateEntry(name, value, isValid) {
+		dispatchFormUpdate(mutateFormInputAction(name, value, isValid));
+	}
+	function deleteEntry(name) {
+		dispatchFormUpdate(deleteFormInputAction(name));
+	}
+	function entries() {
+		const values = {};
+		for (const key in data) {
+			values[key] = data[key].value;
+		}
+		return values;
+	}
+	function get(name) {
+		return data[name];
+	}
+
+	return { mutateEntry, deleteEntry, entries, get };
+};
+export default useForm;
