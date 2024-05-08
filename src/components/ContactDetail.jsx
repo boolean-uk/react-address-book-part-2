67d704956229aea666873fe6d31a846b47a26cc6
Will Baxter <bax.will@googlemail.com>
2024-05-08 16:58:25 +0100

    Made it 0.0005% less ugly

--- src/components/ContactDetail.jsx
+++ src/components/ContactDetail.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
-
 export default function ContactDetail() {
   const [contactToDisplay, setContactToDisplay] = useState({});
   const contactId = Number(useParams().id);
@@ -17,9 +16,11 @@ export default function ContactDetail() {
   }, [contactId]);
 
   return (
-    <div className='detail-card' style={{backgroundColor: `${contactToDisplay.favouriteColour}` }}>
-      
-      <img height={100} src={contactToDisplay.profileImage} />
+    <div
+      className="detail-card p-5 rounded-md flex flex-col gap-5"
+      style={{ backgroundColor: `${contactToDisplay.favouriteColour}` }}
+    >
+      <img width={100} src={contactToDisplay.profileImage} />
       <h2>
         {contactToDisplay.firstName} {contactToDisplay.lastName}
       </h2>
@@ -31,11 +32,15 @@ export default function ContactDetail() {
       <p>Occupation: {contactToDisplay.jobTitle}</p>
       <p>Latitude: {contactToDisplay.latitude}</p>
       <p>Longitude: {contactToDisplay.longitude}</p>
-      <iframe src={`http://maps.google.com/maps?q=${contactToDisplay.longitude},${contactToDisplay.latitude}&z=16&output=embed`} height="400" width="400"></iframe> <br/>
-      <Link to={`/contact-list/edit/${contactId}`}><button>Edit</button></Link>
-      
-
+      <iframe
+        src={`http://maps.google.com/maps?q=${contactToDisplay.longitude},${contactToDisplay.latitude}&z=16&output=embed`}
+        height="400"
+        width="400"
+      ></iframe>{" "}
+      <br />
+      <Link to={`/contact-list/edit/${contactId}`}>
+        <button className="bg-outrun-blue w-20 text-outrun-pink rounded-full">Edit</button>
+      </Link>
     </div>
-    
   );
 }
