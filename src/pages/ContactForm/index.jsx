033c2c0d74cfa0f38317ded40a2ba6374d1f0d89
Angus Townsley <angustownsley@gmail.com>
2024-05-08 20:56:32 +0100

    Add contact post method

--- src/pages/ContactForm/index.jsx
+++ src/pages/ContactForm/index.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react"
+
 export default function ContactForm() {
+
+    const blankForm = {
+        firstName : "",
+        lastName : "",
+        street: "",
+        city: ""
+    }
+    
+    const [formState, setFormState] = useState(blankForm)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(formState)
+          };
+          
+          fetch('https://boolean-api-server.fly.dev/angustownsley/contact/', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => new Error("Failed to add Contact " + err))
+
+        setFormState(blankForm)
+    }
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value} )
+    }
+    
     return(
-        <form>
-            <input type="text" name="firstName" id="firstName"placeholder="First Name..."/>
-            <input type="text" name="lastName" id="lastName" placeholder="Last Name..."/>
-            <input type="text" name="street" id="street" placeholder="Street..."/>
-            <input type="text" name="city" id="city" placeholder="City..."/>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="firstName" placeholder="First Name..." value={formState.firstName} onChange={handleChange}/>
+            <input type="text" name="lastName"  placeholder="Last Name..." value={formState.lastName} onChange={handleChange}/>
+            <input type="text" name="street"  placeholder="Street..." value={formState.street} onChange={handleChange}/>
+            <input type="text" name="city"  placeholder="City..." value={formState.city} onChange={handleChange}/>
             <button type="submit">Submit</button>
         </form>
     )
