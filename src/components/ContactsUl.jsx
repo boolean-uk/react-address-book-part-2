9d609c64fa84106fbef0c33d0ea4bfdb2c44a6bf
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-08 12:40:25 +0200

    add filter by name function

--- src/components/ContactsUl.jsx
+++ src/components/ContactsUl.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 import ContactLi from "./ContactLi"
 
 export default function ContactsUl({ contacts }) {
+    const [search, setSearch] = useState('')
+
+    function handleChange(e) {
+        setSearch(e.target.value)
+    }
+
+    const filteredContacts = contacts.filter((contact) => 
+        contact.firstName.toLowerCase().includes(search.toLocaleLowerCase()) || 
+        contact.lastName.toLowerCase().includes(search.toLocaleLowerCase()))
+
     return (
         <div>
             <h2>Contacts</h2>
+            <input type="search" placeholder="Search" onChange={handleChange} value={search}/>
             <ul className="contacts-ul">
-                {contacts.map((contact, index) => (
+                {filteredContacts.map((contact, index) => (
                     <ContactLi key={index} contact={contact}/>
                 ))}
             </ul>
