ff8ce97b0f49dd8458b849427153f5d1b76bacb3
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-09 14:16:36 +0100

    add readAs prop to specify output - URLobj or base64

--- src/components/Form/FileInput.jsx
+++ src/components/Form/FileInput.jsx
@@ -2,13 +2,39 @@ import React, { useRef, useState } from "react";
 import { FormControl, Image } from "react-bootstrap";
 import placeholderImg from "../../media/user-image-placeholder.png";
 import styles from "./file-input.module.css";
-export default function FileInput({ initialValue, onChange }) {
+
+export default function FileInput({
+	initialValue,
+	onChange,
+	readAs = "URLObj",
+}) {
 	const [src, setSrc] = useState(initialValue || placeholderImg);
 
 	const handleImageUpload = (e) => {
-		const img = URL.createObjectURL(e.target.files[0]);
-		setSrc(img);
-		onChange(img);
+		console.log(readAs);
+		switch (readAs) {
+			case "URLObj": {
+				const img = URL.createObjectURL(e.target.files[0]);
+				setSrc(img);
+				onChange(img);
+				return;
+			}
+			case "base64": {
+				//LINK:https://pqina.nl/blog/convert-a-file-to-a-base64-string-with-javascript/#encoding-the-file-as-a-base-string
+				// Encode the file using the FileReader API
+				const reader = new FileReader();
+				reader.onloadend = () => {
+					// Use a regex to remove data url part
+					const base64String = reader.result
+						.replace("data:", "")
+						.replace(/^.+,/, "");
+					setSrc(base64String);
+					onChange(base64String);
+				};
+				reader.readAsDataURL(e.target.files[0]);
+				return;
+			}
+		}
 	};
 	const ref = useRef(null);
 
