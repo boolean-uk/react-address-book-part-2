e57c8c24443a1f63c7798dde6e7fb312546d3d59
Rafa Lopes <rafalopessecond@gmail.com>
2024-05-09 10:58:13 +0100

    add geolocation icon for IconButton

--- src/components/Buttons/IconButton.jsx
+++ src/components/Buttons/IconButton.jsx
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap";
 export const ICONS = {
 	edit: "fa-regular fa-pen-to-square",
 	trash: "fa-solid fa-trash",
+	geolocation: "fa-solid fa-location-crosshairs",
 };
 
 export default function IconButton({ icon, ...props }) {
