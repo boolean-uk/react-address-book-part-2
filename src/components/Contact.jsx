99c03c9b326fba6cf3f785e6df0ac0467b14092f
Periklis <perik76@gmail.com>
2024-05-10 10:45:54 +0300

    removed details from the list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,21 @@
 import { Link } from "react-router-dom"
 
 export default function Contact({ contact, setContacts }) {
+	
+	//Actual delete
+	// 	function handleDelete() {
+	// 		const isConfirmed = window.confirm(
+	// 			`Are you sure you want to delete ${contact.firstName} ${contact.lastName}?`
+	// 		)
 
+	// 		if (isConfirmed) {
+	// 			fetch(`https://boolean-api-server.fly.dev/PerikK/contact/${contact.id}`, {
+	// 				method: "DELETE",
+	// 			}).then(setContacts)
+	// 		}
+	// 	}
+
+	//Non destructive Delete
 	function handleDelete() {
 		const isConfirmed = window.confirm(
 			`Are you sure you want to delete ${contact.firstName} ${contact.lastName}?`
@@ -17,7 +31,14 @@ export default function Contact({ contact, setContacts }) {
 	}
 
 	return (
-		<li style={{ padding: "30px", border: '2px solid orange',listStyle:'none', width:'300px' }}>
+		<li
+			style={{
+				padding: "30px",
+				border: "2px solid orange",
+				listStyle: "none",
+				width: "300px",
+			}}
+		>
 			<h3>
 				{contact.firstName} {contact.lastName}
 			</h3>
@@ -25,7 +46,9 @@ export default function Contact({ contact, setContacts }) {
 				<button>View Contact</button>
 			</Link>
 			<button>Edit Contact</button>
-			<button onClick={handleDelete}>Delete Contact</button>
+			<Link to='/'>
+				<button onClick={handleDelete}>Delete Contact</button>
+			</Link>
 		</li>
 	)
 }
